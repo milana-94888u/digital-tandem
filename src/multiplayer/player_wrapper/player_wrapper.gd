b1f5ee34b9e91f5ab5320fcc031848cc7e4e64a9
Milana <94888u@gmail.com>
2023-08-15 21:48:05 +0200

    move camera logic to player wrapper

--- src/multiplayer/player_wrapper/player_wrapper.gd
+++ src/multiplayer/player_wrapper/player_wrapper.gd
@@ -2,6 +2,20 @@ extends Node2D
 class_name PlayerWrapper
 
 
+func _ready() -> void:
+	var camera: Camera2D
+	var enabled := false
+	match get_child(0).name:
+		"Mech":
+			camera = get_child(0).get_node("Camera2D")
+			enabled = Server.player_info["role"] == Server.PlayerRole.MECH
+		"Virus":
+			camera = get_child(0)
+			enabled = Server.player_info["role"] == Server.PlayerRole.VIRUS
+	if camera:
+		camera.enabled = enabled
+
+
 @rpc("authority", "call_local", "reliable")
 func change_authority(id: int) -> void:
 #	var camera: Camera2D = get_child(0).get_camera()
--- src/virus/virus.gd
+++ src/virus/virus.gd
@@ -5,13 +5,6 @@ const MIN_ZOOM := 0.1
 const MAX_ZOOM := 2.0
 
 
-func _ready() -> void:
-	if Server.player_info["role"] == Server.PlayerRole.VIRUS:
-		enabled = true
-	else:
-		enabled = false
-
-
 func _physics_process(_delta: float) -> void:
 	position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 10
 	if Input.is_action_pressed("zoom_in"):

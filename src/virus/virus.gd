bcc9bbf611b1b3ad1968f921a5edd62eecd6cb28
Milana <94888u@gmail.com>
2023-08-15 17:40:55 +0200

    implement virus camera zoom

--- src/virus/virus.gd
+++ src/virus/virus.gd
@@ -1,6 +1,10 @@
 extends Camera2D
 
 
+const MIN_ZOOM := 0.1
+const MAX_ZOOM := 2.0
+
+
 @rpc("authority", "call_local", "reliable")
 func change_authority(id: int) -> void:
 	set_multiplayer_authority(id)
@@ -17,3 +21,7 @@ func _enter_tree() -> void:
 func _physics_process(_delta: float) -> void:
 	if is_multiplayer_authority():
 		position += Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 10
+		if Input.is_action_pressed("zoom_in"):
+			zoom = Vector2.ONE * minf(MAX_ZOOM, zoom.x + 0.02)
+		elif Input.is_action_pressed("zoom_out"):
+			zoom = Vector2.ONE * maxf(MIN_ZOOM, zoom.x - 0.02)
